412da1358d416192c63db259816462d8cd799884
Christian-Swapnil <christianswapnil23@gmail.com>
2022-06-01 20:22:24 +0530

    commit

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -8,6 +8,16 @@ class MyLogin extends StatefulWidget {
 }
 
 class _MyLoginState extends State<MyLogin> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  void validate() {
+    if (formkey.currentState!.validate()) {
+      print("ok");
+    } else {
+      print("Error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -35,83 +45,94 @@ class _MyLoginState extends State<MyLogin> {
                     top: MediaQuery.of(context).size.height * 0.5,
                     right: 40,
                     left: 40),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          fillColor: Colors.white54,
-                          filled: true,
-                          hintText: 'Email',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          fillColor: Colors.white54,
-                          filled: true,
-                          hintText: 'Password',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Sign In',
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                        ),
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.white12,
-                          child: IconButton(
-                            color: Colors.white,
-                            onPressed: () {
-                              Navigator.pushNamed(context, "index");
-                            },
-                            icon: Icon(Icons.arrow_forward),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  key: formkey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                            fillColor: Colors.white54,
+                            filled: true,
+                            hintText: 'Email',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return "Required *";
+                          } else if (val.length < 5) {
+                            return "Bro Type More than 5";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            fillColor: Colors.white54,
+                            filled: true,
+                            hintText: 'Password',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Sign In',
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
                           ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, 'register');
-                            },
-                            child: Text(
-                              'Sign Up',
-                              style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  color: Colors.black,
-                                  fontSize: 18),
-                            )),
-                        TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Forget Password',
-                              style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  color: Colors.black,
-                                  fontSize: 18),
-                            )),
-                      ],
-                    )
-                  ],
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.white12,
+                            child: IconButton(
+                              color: Colors.white,
+                              onPressed: validate,
+                              icon: Icon(Icons.arrow_forward),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, 'register');
+                              },
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    color: Colors.black,
+                                    fontSize: 18),
+                              )),
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Forget Password',
+                                style: TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    color: Colors.black,
+                                    fontSize: 18),
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             )
